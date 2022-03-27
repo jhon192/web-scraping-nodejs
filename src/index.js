2fd186a14bde8b5372800e36414dbb1bb2d27a1b
jhon192 <jhon19.2@outlook.com>
2022-03-26 21:39:39 -0400

    index file updated

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import pw from "playwright"
 import fs from 'fs';
 
-const main = async () => {
+const main = async (text) => {
     const browser = await pw.firefox.launch({
         headless: true
     }) 
 
     const page = await browser.newPage();
-    await page.goto("https://www.mangatigre.com/")
-    const data = await page.$$eval(".popular-box", value => {
+    await page.goto(`https://translate.google.com/?sl=es&tl=en&text=${text}&op=translate`)
+    const data = await page.$$eval(".J0lOec", value => {
         const data = []
-        value.forEach(datos => {
-            const animeName = datos.querySelector(".name").innerText;
-            data.push(animeName + "\n")
+        value.forEach((datos) => {
+            const translateData = datos.querySelector("span").innerText;
+            data.push(translateData + ".")
         })
 
         return data
 
     })
 
-    console.log(data);
-    fs.writeFile("animeList.txt", `${data}\n`, "utf-8", err => err)
-    await page.waitForTimeout(5000)
+    data.map(x => {
+      console.log(x);  
+    });
+    // fs.writeFile("animeList.txt", `${data}\n`, "utf-8", err => err)
+    await page.waitForTimeout(1000)
     browser.close()
 }
 
-main()
+main("Que haremos hoy?")
